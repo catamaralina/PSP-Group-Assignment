8ddd98eb82e484e370d7cceacd61fa8b75cd7600
catamaralina <tali.jonker@gmail.com>
2023-11-23 19:46:37 -0500

    Test run

---
+++ Implementation.py
@@ -0,0 +1,8 @@
+#Water Budget
+#Using the open source data from Grand River Watershed
+#to determine statistical calculations in a select month. 
+#Calculate average and compare it to other stations 
+#in the watershed
+
+#Assumed constant
+#Runoff Coefficient assumed to be .25
