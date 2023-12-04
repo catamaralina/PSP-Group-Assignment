65798759b2b456ec9b1158def43d523b7c3a94a3
catamaralina <tali.jonker@gmail.com>
2023-12-04 11:34:13 -0500

    Update code to work with csv

--- Implementation.py
+++ Implementation.py
@@ -7,78 +7,85 @@
 #Assumed constant
 #Runoff Coefficient assumed to be .25
 
-#--FUNCTIONS--
-#-------------
-
 
 #--DATA FROM CSV--
 #-----------------
-#import data
+#Import Data
+import csv
+rows = []
+header = []
+with open("Complete_Table.csv", 'r') as file:
+    csvreader = csv.reader(file)
+    header = next(csvreader)
+    print(header)
+    for row in csvreader:
+        rows.append(row)
+list = list(rows)
 
-list = [[id], [station], [month], [flow 18], [flow22], [precip18], [precip22], [areaha]]
+#list = [[id], [station], [month], [flow 18], [flow22], [precip18], [precip22], [areaha]]
+#         [0]   [1]         [2]     [3]         [4]         [5]         [6]         [7]
 
+#Again = 1 for looping purposes. 
+again = 1 
 
-#drainage area (ha)
-#Climate Station [Luther Marsh, Guelph Lake, Laurel Creek, York, Rd 32 WQ]
-#                [Legatt, Victoria Road, Erbsville, York, Rd 32 WQ Stn]
-#County          [Dufferin, Guelph/Eramosa, Waterloo, Haldimand, Wellington]
-DrainageArea = [148677, 29284, 6406, 125045, 266536]
+while again == 1: 
+    #--USER INPUT--
+    #--------------
 
-#--USER INPUT--
-#--------------
-selected_station = input("Select a station (1-5, see map for reference): ")
-selected_month = input("Select a month (1-12): ")
+    #ADD IN HANDLERS FOR INPUT VALUES THAT ARE NOT INTEGERS
+    selected_station = int(input("Select a station (1-5, see map for reference): "))
+    selected_month = int(input("Select a month (1-12): "))-1
 
-#--CALCULATIONS--
-#----------------
-#HOURS IN MONTHS
+    ##Obtaining the station specific records
+    record = []
+    for i in list:
+        index = selected_station*12-12
+        start = (selected_station-1)*12
+        stop = (selected_station)*12
+        record = list[start:stop]
 
+    #--CALCULATIONS--
+    #----------------
+    #MONTHLY AVERAGE FLOW, PRECIPITATION
+    flow_2018 = float(record[selected_month][3])
+    flow_2022 = float(record[selected_month][4])
+    precip_2018 = float(record[selected_month][5])
+    precip_2022 = float(record[selected_month][6])
 
-#----------------
-#RUNOFF (Roseke, 2013)
-# C = Runoff coefficient = .25 
-# i = Rainfall intensity (mm/hr)
-# A = Drainage area (ha)
-# Q = Runoff (m^3/s)
-
-C = .25
-# Ri = 0
-# A = DrainageArea[(selected_station-1)] #grabs indexed station area
-# Q = .00278*C*i*A
-
-record = []
-
-for i in range (len(list)):
-    #get the station specific records
-    record = record + list[i][(selected_station-1)*12:(selected_station)*12]
-
-for j <=12: 
-
-#MONTHLY AVERAGE FLOW, PRECIPITATION
-flow_2018 = record[3][selected_month]
-flow_2022 = record[4][selected_month]
-precip_2018 = record[5][selected_month]
-precip_2022 = record[6][selected_month]
-DrainageArea = record[7][selected_month]
-runoff_2018 = .00278*C*precip_2018*DrainageArea
-runoff_2022 = .00278*C*precip_2022*DrainageArea
-#CHANGE
-change_flow = flow_2018-flow_2022
-change_precip = precip_2018-precip_2022
-change_runoff = runoff_2018-runoff_2022
-
+    #RUNOFF (Roseke, 2013)
+    # C = Runoff coefficient = .25 
+    # i = Rainfall intensity (mm/hr)
+    # A = Drainage area (ha)
+    # Q = Runoff (m^3/s)
+    # Q = .00278*C*i*A
+    C = float(.25)
+    DrainageArea = float(record[selected_month][7])
+    runoff_2018 = .00278*C*precip_2018*DrainageArea
+    runoff_2022 = .00278*C*precip_2022*DrainageArea
 
 
 
-#--OUTPUT--
-#----------
+    #CHANGE
+    change_flow = flow_2018-flow_2022
+    change_precip = precip_2018-precip_2022
+    change_runoff = runoff_2018-runoff_2022
 
-print("Runoff coefficient in the Grand River watershed is assumed to be .25")
-print("Selected station: ", selected_station)
-print("Selected month: ", selected_month)
-print('year\t\tFlow\t\tPrecipitation\t\tRunoff')
-print('2018\t\t' + flow_2018 + '\t\t' + precip_2018  + '\t\t' + runoff_2018)
-print('2022\t\t' + flow_2022 + '\t\t' + precip_2022 + '\t\t' + runoff_2022)
-print("'18-'22\t\t" + change_flow + '\t\t' + change_precip + '\t\t' + change_runoff)
+    #--OUTPUT--
+    #----------
+    #Maybe add units?
+    print("Runoff coefficient in the Grand River watershed is assumed to be .25")
+    print("Selected station: ", selected_station)
+    print("Selected month: ", (selected_month+1)) #Took a -1 at the beginning for indexing purposes. 
+    print('year\t\tFlow(m^3/s)\t\tPrecipitation(mm/hr)\t\tRunoff(m^3/s)')
+    print('2018\t\t' + str(flow_2018) + '\t\t' + str(precip_2018)  + '\t\t' + str(runoff_2018))
+    print('2022\t\t' + str(flow_2022) + '\t\t' + str(precip_2022) + '\t\t' + str(runoff_2022))
+    print("'18-'22\t\t" + str(change_flow) + '\t\t' + str(change_precip) + '\t\t' + str(change_runoff))
 
-again = input('Would you like to select another month or location to display? (y/n) ')
+    #Ask user if they want to go again
+    again = input('Would you like to select another month or location to display? (y/n) ')
+    if again.lower() == 'y':
+        again = 1
+    elif again.lower() == 'n':
+        again = 0
+    else:
+        print("please print y or n")
